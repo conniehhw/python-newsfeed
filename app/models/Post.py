236e331236bd05611cb525e539f0ed6cbe0cf7e2
Connie Wong <conniehhw@gmail.com>
2023-04-21 15:48:08 -0400

    Set up db connection in Flask, pass User data into homepage templates, create & implement custom filters

--- app/models/Post.py
+++ app/models/Post.py
@@ -12,9 +12,9 @@ class Post(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-  vote_count = column_property(
-    select([func.count(Vote.id)]).where(Vote.post_id == id)
-  )
+  # vote_count = column_property(
+  #   select([func.count(Vote.id)]).where(Vote.post_id == id)
+  # )
 
   user = relationship('User')
   comments = relationship('Comment', cascade='all,delete')
